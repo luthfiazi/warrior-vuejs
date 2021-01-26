057e18b016add8ffe5056f9b9d2f4e903e481f39
luthfiazzz <luthfiazizi11@gmail.com>
2021-01-26 14:16:40 +0700

    update ui

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,292 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+      <a class="navbar-brand" href="#"><img src="img/warrior.png" width="80px"/></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+          <a class="nav-item nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Talent</a>
+              <a class="nav-item nav-link" href="#">About us</a>
+              <a class="nav-item nav-link" href="#"><button class="btn btn-danger">Login</button></a>
+        </div>
+      </div>
+    </div>
+  </nav>
+   <div class="jumbotron">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6 text-left">
+          <h1 class="display-4 mt-5">
+            We Provide <span> <br>a Great Talent</span> 
+          </h1>
+          <h3 class="mb-5">a growth mindset that will give solutions</h3>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="E-mail adress"  aria-describedby="button-addon2">
+            <div class="input-group-append">
+              <button class="btn btn-danger" type="button" id="button-addon2">subscribe</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-10 info-panel">
+        <h3 class="text-center"><b>Most Wanted</b></h3>
+        <div class="row mx-4">
+          <div class=" mx-2" style="width: 16rem;">
+            <img src="img/Group 1.png"  class="card-img-top" alt="...">
+            <div class="card-body">
+            </div>
+          </div>
+          <div class=" mx-2 " style="width: 16rem;">
+            <img src="img/Group 2.png"  class="card-img-top" alt="...">
+            <div class="card-body">
+            </div>
+          </div>
+          <div class=" mx-2 " style="width: 16rem;">
+            <img src="img/Group 3.png"  class="card-img-top" alt="...">
+            <div class="card-body">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+   <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-10 info-panel-card">
+          <h3 class="text-center"><b>Our Partner</b></h3>
+          <br>
+          <br>
+          <div class="container text-center my-3">
+            <div class="row mx-auto my-auto d-block">
+                <div id="recipeCarousel" class="carousel slide w-100" data-ride="carousel">
+                    <div class="carousel-inner w-100 p-3" role="listbox">
+                        <div class="carousel-item active ">
+                            <div class="col-md-4">
+                                <div class="">
+                                  <img src="img/tokped 1.png" class="d-block w-5 mb-5" width="200" alt="...">
+                                  </div>
+                                  
+                            </div>
+                        </div>
+                        <div class="carousel-item ">
+                            <div class="col-md-4">
+                                <div class="">
+                                  <img src="img/shopee 1.png" class="d-block w-5 mb-5" width="200" alt="...">
+                                  </div>
+                                 
+                            </div>
+                        </div>
+                        </div>
+                        <div class="carousel-item ">
+                            <div class="col-md-4">
+                                <div class="">
+                                  <img src="img/gojek 1.png" class="d-block w-5 mb-5" width="200" alt="...">
+                                  </div>
+                                 
+                            </div>
+                        </div>
+                        </div>
+                        <div class="carousel-item ">
+                            <div class="col-md-4">
+                                <div class="">
+                                  <img src="img/grab 1.png" class="d-block w-5 mb-5" width="200" alt="...">
+                                  </div>
+                                  
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev w-auto" href="#recipeCarousel" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next w-auto" href="#recipeCarousel" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+        
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-6 info-panel-card mx-3">
+        <h3 class="text-center"><b>Top Talent</b></h3>
+        <br>
+        <br>
+        <div class="container text-center my-3">
+          <div class="row mx-auto my-auto d-block">
+              <div id="recipeCarousel" class="carousel slide w-100" data-ride="carousel">
+                  <div class="carousel-inner w-100 p-3" role="listbox">
+                      <div class="carousel-item active  ">
+                          <div class="col-4 ">
+                              <div class=" card-body ">
+                                <img src="img/Ellipse 1.png"  width="60" alt="...">
+                                </div>
+                                <p>Steve Job</p>
+                          </div>
+                      </div>
+                      <div class="carousel-item ">
+                          <div class="col-md-4">
+                              <div class=" card-body">
+                                <img src="img/Ellipse 2.png"  width="60" alt="...">
+                                </div>
+                                <p>Ellon Musk</p>
+                          </div>
+                      </div>
+                      </div>
+                      <div class="carousel-item ">
+                          <div class="col-md-4">
+                              <div class=" card-body">
+                                <img src="img/Ellipse 3.png" class="d-block" width="60" alt="...">
+                                </div>
+                                <p>Linus Lerdov</p>
+                          </div>
+                      </div>
+                      </div>
+                      <div class="carousel-item ">
+                          <div class="col-md-4">
+                              <div class=" card-body">
+                                <img src="img/Ellipse 4.png" class="d-block" width="60" alt="...">
+                                </div>
+                                <p>Nam Do San</p>
+                          </div>
+                      </div>
+                  </div>
+                  <a class="carousel-control-prev w-auto" href="#recipeCarousel" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next w-auto" href="#recipeCarousel" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                  </a>
+              </div>
+            </div>
+            <div class=" col-3 info-panel-hakathon">
+              <h3 class="text-center"><b>Event Hackathon</b></h3>
+              <br>
+              <br>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="card  h-25">
+                    <div class="d-flex justify-content-between">
+                      <img src="img/Rectangle 11.png" alt="...">
+                        <div>
+                          <h6 class="text-left">East Venture</h6>
+                        <h6 class="text-left">10 Januari 2021</h6>
+                        </div>
+                        <button class="btn btn-sm btn-danger h-25">Join</button>
+                    </div>
+                </div>
+                </div>
+             
+              </div>
+            </div>
+      </div>
+       
+      </div>
+     
+    </div>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-10 info-panel-volunteer mx-5">
+        <h3 class="text-center text-white"><b>Open Source Volunteer</b></h3>
+        <br>
+        <br>
+        <div class="container text-center my-3">
+          <div class="row mx-auto my-auto d-block">
+              <div id="recipeCarousel" class="carousel slide w-100" data-ride="carousel">
+                  <div class="carousel-inner w-100 p-3" role="listbox">
+                      <div class="carousel-item active ">
+                          <div class="col-md-4">
+                              <div class="card card-body">
+                                  <img class="img-fluid d-block" src="img/Rectangle 15.png">
+                                </div>
+                                <p class="text-white">Golang.id</p>
+                          </div>
+                      </div>
+                      <div class="carousel-item ">
+                          <div class="col-md-4">
+                              <div class="card card-body">
+                                  <img class="img-fluid d-block" src="img/Rectangle 18.png">
+                                </div>
+                                <p class="text-white">Reactjs.id</p>
+                          </div>
+                      </div>
+                  </div>
+                  <a class="carousel-control-prev w-auto" href="#recipeCarousel" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next w-auto" href="#recipeCarousel" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                  </a>
+              </div>
+            </div>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="jumbotron jumbotron-fluid mt-5">
+    <div class="container">
+      <div class="row">
+        <div class="row">
+          <div class="col-md-6">
+            <p>Main Menu</p>
+            <a href="#">Home</a>
+            <br>
+            <a href="#">Talent</a>
+            <br>
+            <a href="#">About us</a>
+          </div>
+          <div class="col-md-6">
+            <p>Event</p>
+            <a href="#">Open Source</a>
+            <br>
+            <a href="#">Hackathon</a>
+          </div>
+          <div class="col-md-6">
+            <p>Partner</p>
+            <a href="#">Volunteer</a>
+            <br>
+            <a href="#">Company</a>
+            <br>
+            <a href="#">Term of Services</a>
+            <br>
+            <a href="#">Privacy Policy</a>
+          </div>
+        </div>
+        <div class="col-sm-6 text-left">
+          <h1 class="display-4 mt-5">
+            <a class="navbar-brand" href="#"><img src="img/warrior.png" width="100px"> Social</a>
+      </h1>
+      <div class="mt-5">
+        <img src="img/Ellipse 8.png" width="50px" alt="">
+        <img src="img/Ellipse 9.png" width="50px" alt="">
+        <img src="img/Ellipse 10.png" width="50px" alt="">
+        <img src="img/Ellipse 11.png" width="50px" alt="">
+      </div>
+    </div>
+        <div class="col-sm-6 text-left">
+          <!-- <h1 class="display-4 mt-5">
+            "Growth Your Business With a Great ManPower"
+      </h1> -->
+    </div>
+  </div>
+  </div>
+  </div>
+    <footer class="container text-center">
+      <p>&copy; Copyright warrior 2020.</p>
+    </footer>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import '../dist/css/style.css'
+
+
 createApp(App).mount('#app')
